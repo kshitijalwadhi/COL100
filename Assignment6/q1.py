e4a9660677c38948433d492cc6497b345ad83055
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-03-31 13:26:40 +0530

    add invariants and time complexity for q1

--- Assignment6/q1.py
+++ Assignment6/q1.py
@@ -1,3 +1,4 @@
+# assert: for n>1, elements of arr[1...n] are in ascending order
 def removeduplicates(arr, n):
     if (n == 0 or n == 1):
         return n
@@ -5,21 +6,42 @@ def removeduplicates(arr, n):
     temp[0] = arr[0]
     count = 1
     for i in range(1, n):
+        # INV: after the ith iteration (count<=i), temp[1...count] contains all unique elements in ascending order.
         if (arr[i] != arr[i-1]):
             temp[count] = arr[i]
             count += 1
+    # comment: next loop for adding the 0's at the end of array for repeated elements
     for i in range(count, n):
         temp[i] = 0
+    # assert: replacing arr with temp now
     for i in range(0, n):
         arr[i] = temp[i]
     return count
+# assert: array elements arr[1...count] are unique and after that there are 0's
+# and count is the number of unique elements in the original array.
 
+
+# Time Complexity:
+
+# for i in range(1, n):
+#     if (arr[i] != arr[i-1]):
+#         temp[count] = arr[i]
+#         count += 1
+
+# This loop increments the value of i by 1 after each execution.
+# So the complexity is linear.
+# T(n) = T(n-1) + 1
+
+# Therefore Time Complexity is: O(N)
+
+
+# Code for input and output to test program
 
 arr = [2, 2, 4, 8, 8, 23, 37, 37, 42]
 n = len(arr)
-newn = removeduplicates(arr, n)
+newN = removeduplicates(arr, n)
 
 for i in range(n):
     print(arr[i], end=' ')
 print()
-print("New length is: ", newn)
+print("New length is: ", newN)
